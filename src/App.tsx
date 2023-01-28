be5eadb1b5aeda217b65b6b1c8e79ca4b5db9733
enginbukum <yasengin2005@gmail.com>
2023-01-28 18:25:04 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { add, remove, toggleCompleted } from "./features/todoSlice";
+import { useAppDispatch, useAppSelector } from "./store";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const todos = useAppSelector((state) => state.todos);
+
+  const dispatch = useAppDispatch();
+
+  const onSave = () => {
+    dispatch(add(title));
+    setTitle("");
+  };
+
+  const toggle = (id: string) => {
+    dispatch(toggleCompleted(id));
+  };
+
+    const onDelete = (id: string) => {
+      dispatch(remove(id));
+    };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.currentTarget.value)}
+      />
+      <button onClick={onSave}>Save</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <button onClick={() => toggle(todo.id)}>
+              {todo.completed ? "Completed" : "Not Completed"}
+            </button>
+            <button onClick={() => onDelete(todo.id)}>Delete</button>
+            <span>{todo.title}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
